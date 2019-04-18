f73e4d6a229caa0bac2c24368c7f8adec7bdba27
Daniel <alejo.dafr@gmail.com>
2019-04-18 15:50:57 -0500

    Working in the GUI

--- src/ui/Main.java
+++ src/ui/Main.java
@@ -3,6 +3,23 @@ package ui;
 import javafx.application.Application;
 import javafx.stage.Stage;
 
+/**<b>Critical note:</b><br>
+ * <div>Icons made by <a href="https://www.flaticon.com/authors/chanut" title="Chanut">Chanut</a> from <a href="https://www.flaticon.com/" 		   
+ * title="Flaticon">www.flaticon.com</a> is licensed by <a href="http://creativecommons.org/licenses/by/3.0/" 		    
+ * title="Creative Commons BY 3.0" target="_blank">CC 3.0 BY</a></div>
+ * <br>
+ * <div>Icons made by <a href="https://www.flaticon.com/authors/smashicons" title="Smashicons">Smashicons</a> from <a href="https://www.flaticon.com/" 			    
+ * title="Flaticon">www.flaticon.com</a> is licensed by <a href="http://creativecommons.org/licenses/by/3.0/" 			    
+ * title="Creative Commons BY 3.0" target="_blank">CC 3.0 BY</a></div>
+ * <br>
+ * <div>Icons made by <a href="https://www.flaticon.com/authors/daniel-bruce" title="Daniel Bruce">Daniel Bruce</a> from <a href="https://www.flaticon.com/" 			    
+ * title="Flaticon">www.flaticon.com</a> is licensed by <a href="http://creativecommons.org/licenses/by/3.0/" 			    
+ * title="Creative Commons BY 3.0" target="_blank">CC 3.0 BY</a></div>
+ * <br>
+ * <div>Icons made by <a href="https://www.freepik.com/" title="Freepik">Freepik</a> from <a href="https://www.flaticon.com/" 			    
+ * title="Flaticon">www.flaticon.com</a> is licensed by <a href="http://creativecommons.org/licenses/by/3.0/" 			    
+ * title="Creative Commons BY 3.0" target="_blank">CC 3.0 BY</a></div>
+ * */
 public class Main extends Application {
 
 	public static void main(String[] args) {
