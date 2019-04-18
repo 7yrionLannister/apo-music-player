4911d63f39fa2a444a9f87f213f69e332aae1627
Daniel Fernandez <alejo.dafr@gmail.com>
2019-04-18 17:38:14 -0500

    Still working in the GUI

--- src/ui/Main.java
+++ src/ui/Main.java
@@ -19,6 +19,10 @@ import javafx.stage.Stage;
  * <div>Icons made by <a href="https://www.freepik.com/" title="Freepik">Freepik</a> from <a href="https://www.flaticon.com/" 			    
  * title="Flaticon">www.flaticon.com</a> is licensed by <a href="http://creativecommons.org/licenses/by/3.0/" 			    
  * title="Creative Commons BY 3.0" target="_blank">CC 3.0 BY</a></div>
+ * <br>
+ * <div>Icons made by <a href="https://www.flaticon.com/authors/eleonor-wang" title="Eleonor Wang">Eleonor Wang</a> from <a href="https://www.flaticon.com/" 			    
+ * title="Flaticon">www.flaticon.com</a> is licensed by <a href="http://creativecommons.org/licenses/by/3.0/" 			    
+ * title="Creative Commons BY 3.0" target="_blank">CC 3.0 BY</a></div>
  * */
 public class Main extends Application {
 
