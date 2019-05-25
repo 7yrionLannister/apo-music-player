d4718b1b408503d98ea28308b135966728100579
7yrionLannister <alejo.DAFR@gmail.com>
2019-05-25 00:02:06 -0500

    Sorting algorithms implemented but the test have not been done

--- src/model/MusicFolder.java
+++ src/model/MusicFolder.java
@@ -3,6 +3,7 @@ import java.io.File;
 import java.io.IOException;
 import java.io.Serializable;
 import java.util.ArrayList;
+import java.util.Collections;
 
 public class MusicFolder implements Serializable {
 	private File folder;
@@ -93,23 +94,55 @@ public class MusicFolder implements Serializable {
 	}
 	
 	public void sortSongsByTitle() {
-		//TODO implement this (Collections.sort(natural order))
+		Collections.sort(songs);
 	}
 	
+	//it uses bubble sort
 	public void sortSongsByArtist() {
-		//TODO implement this (selection sort)
+		ArtistComparator ac = new ArtistComparator();
+		for(int i = 0; i < songs.size(); i++) {
+			for(int j = 0; j < songs.size()-1-i; j++) {
+				if(ac.compare(songs.get(j), songs.get(j+1)) > 0) {
+					Song temp = songs.get(j);
+					songs.set(j, songs.get(j+1));
+					songs.set(j+1, temp);
+				}
+			}
+		}
 	}
 	
+	//it uses insertion sort
 	public void sortSongsByAlbum() {
-		//TODO implement this (bubble sort)
+		AlbumComparator ac = new AlbumComparator();
+		for(int i = 1; i < songs.size(); i++) {
+			Song current = songs.get(i);
+			int j = i-1;
+			while(j >= 0 && ac.compare(songs.get(j), current) > 0) {
+				songs.set(j+1, songs.get(j));
+				j--;
+			}
+			songs.set(j+1, current);
+		}
 	}
 	
+	//it uses selection sort
 	public void sortSongsBySize() {
-		//TODO implement this (insertion sort)
+		SizeComparator sc = new SizeComparator();
+		for(int i = 0; i < songs.size()-1; i++) {
+			int low = i;
+			for(int j = i+1; j < songs.size(); j++) {
+				if(sc.compare(songs.get(low), songs.get(j)) > 0) {
+					low = j;
+				}
+			}
+			Song temp = songs.get(low);
+			songs.set(low, songs.get(i));
+			songs.set(i, temp);
+		}
 	}
 	
 	public void sortSongsByGenre() {
-		//TODO implement this (Collections.sort(non-natural order))
+		Collections.sort(songs, new GenreComparator());
 	}
 	
 	public ArrayList<Song> getSongs(){
