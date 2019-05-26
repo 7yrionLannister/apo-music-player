30dc20f66ced15029ac91d7e6e1b0611ea0db254
Camilo Enriquez <rot7447@gmail.com>
2019-05-25 23:38:47 -0500

    TitleComparator ready

--- src/model/GenreComparator.java
+++ src/model/GenreComparator.java
@@ -7,7 +7,7 @@ public class GenreComparator implements Comparator<Song>{
 	/** This method compares two song genres lexicographically.
 	 * @param s1 The Song that will compare its genre with the Song s2 genre lexicographically.
 	 * @param s2 The Song that will be compared with Song s1 genre lexicographically.
-	 * @return An integer that represents if the Song s1 artist is greater, less or equal to Song s2 genre lexicographically.  
+	 * @return An integer that represents if the Song s1 genre is greater, less or equal to Song s2 genre lexicographically.  
 	 */
 	@Override
 	public int compare(Song o1, Song o2) {
--- src/model/TitleComparator.java
+++ src/model/TitleComparator.java
@@ -3,7 +3,12 @@ package model;
 import java.util.Comparator;
 
 public class TitleComparator implements Comparator<Song>{
-
+	
+	/** This method compares two song titles lexicographically.
+	 * @param s1 The Song that will compare its title with the Song s2 title lexicographically.
+	 * @param s2 The Song that will be compared with Song s1 title lexicographically.
+	 * @return An integer that represents if the Song s1 title is greater, less or equal to Song s2 title lexicographically.  
+	 */
 	@Override
 	public int compare(Song o1, Song o2) {
 		return o1.getTitle().compareTo(o2.getTitle());
