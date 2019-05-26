fbeefce660853c730b588e344d795bc1008c6b63
Camilo Enriquez <rot7447@gmail.com>
2019-05-26 18:17:57 -0500

    CustomExceptions javadoc ready

--- src/customExceptions/AttemptedToRemoveDemoLibraryException.java
+++ src/customExceptions/AttemptedToRemoveDemoLibraryException.java
@@ -1,6 +1,10 @@
 package customExceptions;
 
 public class AttemptedToRemoveDemoLibraryException extends Exception {
+	/**
+	 * This method throws a message in console indicating that the user tried to remove the default music folder.
+	 * @see <a href="https://docs.oracle.com/javase/8/docs/api/java/lang/Throwable.html#getMessage--" title="getMessage">getMessage</a> in class Throwable.
+	 */
 	@Override
 	public String getMessage() {
 		return "Unable to remove demo library, permission denied";
