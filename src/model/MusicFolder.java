ce4833c3e544fac977cddee8fdfbd28ffb49ec2b
Daniel Fernandez <alejo.dafr@gmail.com>
2019-05-28 19:14:03 -0500

    Trying to fix problem with the boolean that indicates if the musicfolder is sorted, it is always true and it is not suposed to be like that

--- src/model/MusicFolder.java
+++ src/model/MusicFolder.java
@@ -68,6 +68,7 @@ public class MusicFolder implements Serializable {
 				addSongToBST(root, new Song(content[i]));
 			}
 		}
+		sortedByTitle = true;
 		songs = inorder();
 		folderName = folder.getName();
 		numberOfSongs = songs.size();
@@ -123,7 +124,6 @@ public class MusicFolder implements Serializable {
 	
 	public ArrayList<Song> preorder() {
 		ArrayList<Song> preorderSongs = new ArrayList<Song>();
-		sortedByTitle = true;
 		if(root != null) {
 			preorder(root, preorderSongs);
 		}
@@ -278,9 +278,9 @@ public class MusicFolder implements Serializable {
 
 	public Song search(String title) {
 		Song match = null;
-		if(sortedByTitle) {
+		if(sortedByTitle) {System.out.println("en el arreglo");
 			match = searchInArrayList(title);
-		} else {
+		} else {System.out.println("en el arbol");
 			match = searchInBinarySearchTree(root, title);
 		}	
 		return match;
